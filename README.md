6d1a307228f631cd88b5ee642ea7e0063263453a
Diogo Fernandes <diogofernandesop@gmail.com>
2021-05-12 21:51:04 -0300

    Update button and colors

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# sound-spectrum
+# Sound Spectrum
 The sound spectrum made in javascript
+
+### Example
+![](example.png?raw=true)
+
+### Autores
+* [Diogo Fernandes](https://github.com/dfop02)
--- js/sound.js
+++ js/sound.js
@@ -1,5 +1,8 @@
-window.onload = function() {
+let angle = Math.random() * 360;
+const angleDlt = 60;
+const step = 0.004;
 
+window.onload = function() {
   var file  = document.getElementById("file");
   var audio = document.getElementById("audio");
 
@@ -9,16 +12,19 @@ window.onload = function() {
     audio.load();
     audio.play();
 
-    audio.classList.add("playing");
+    audio.style.display = 'block'
+    audio.classList.add('playing');
 
     var context  = new AudioContext();
     var src      = context.createMediaElementSource(audio);
     var analyser = context.createAnalyser();
 
-    var canvas    = document.getElementById("canvas");
+    var canvas    = document.getElementById('canvas');
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
-    var ctx       = canvas.getContext("2d");
+    var ctx       = canvas.getContext('2d');
+
+    canvas.style.display = 'block';
 
     src.connect(analyser);
     analyser.connect(context.destination);
@@ -37,6 +43,15 @@ window.onload = function() {
     var barHeight;
     var x = 0;
 
+    function createGradient() {
+      var gr = ctx.createLinearGradient(0, 0, WIDTH, 0);
+      gr.addColorStop(0,   `hsl(${(angle % 360)}, 100%, 50%)`);
+      gr.addColorStop(0.5, `hsl(${((angle + (angleDlt/2)) % 360)}, 100%, 50%)`);
+      gr.addColorStop(1,   `hsl(${((angle + angleDlt) % 360)}, 100%, 50%)`);
+      ctx.fillStyle = gr;
+      angle += step;
+    }
+
     function renderFrame() {
       requestAnimationFrame(renderFrame);
 
@@ -44,17 +59,22 @@ window.onload = function() {
 
       analyser.getByteFrequencyData(dataArray);
 
-      ctx.fillStyle = "#000";
+      ctx.fillStyle = '#000';
       ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
       for (var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
 
-        var r = barHeight + (25 * (i/bufferLength));
-        var g = 250 * (i/bufferLength);
-        var b = 50;
+        // var r = barHeight + (25 * (i/bufferLength));
+        // var g = 250 * (i/bufferLength);
+        // var b = 50;
 
-        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        // var r = i * barHeight + (50 * (i/bufferLength));
+        // var g = barHeight * (i/bufferLength);
+        // var b = barHeight * (i/bufferLength);
+
+        // ctx.fillStyle = `rgb(${r},${g},${b})`;
+        createGradient();
         ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
 
         x += barWidth + 1;
